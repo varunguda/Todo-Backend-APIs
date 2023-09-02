27e8654d92fa958d96b12008f139b9548797d33f
Varun Sai Guda <93998822+varunguda@users.noreply.github.com>
2023-09-03 02:32:59 +0530

    feat-validation

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { createUser, deleteUser, getUser, loginUser, logoutUser, updateUser } from "../controllers/users.js";
 import { isAuthenticated } from "../middleware/auth.js";
+import { userDataValidation } from "../middleware/validation.js";
 
 const router = Router();
 
-router.post('/createuser', createUser);
+router.post('/createuser', userDataValidation, createUser);
 
-router.get('/login', loginUser)
+router.get('/login', userDataValidation ,loginUser)
 
 router.get('/getuser', isAuthenticated, getUser);
 
-router.put('/updateuser', isAuthenticated, updateUser);
+router.put('/updateuser', isAuthenticated, userDataValidation, updateUser);
 
 router.delete('/deleteuser', isAuthenticated, deleteUser);
 
